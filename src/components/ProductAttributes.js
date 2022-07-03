83b05add638ea2133a7ac14b9d90b03afea396ba
gralmeidan <gralmeidan@gmail.com>
2022-07-03 11:53:26 -0400

    Made productAttributes table expandable

--- src/components/ProductAttributes.js
+++ src/components/ProductAttributes.js
@@ -1,21 +1,43 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import './ProductAttributes.css';
+import ArrowDown from '@mui/icons-material/ArrowDropDownRounded';
+
 
 class ProductAttributes extends React.Component {
+  state = {
+    display: false,
+  };
+
   render() {
+    const { display } = this.state;
     const { attributes } = this.props;
     return (
-      <table id="attributes-table" className="mx-1">
-        <tbody>
-          { attributes.map(({ name, value_name, id }) => (
-            <tr key={id}>
-              <td>{name}</td>
-              <td>{value_name}</td>
-            </tr>
-          )) }
-        </tbody>
-      </table>
+      <div className="flex flex-col border-[1px] border-neutral-400
+      m-1 rounded-md" id="attributes-table">
+        <button
+          onClick={() => this.setState(({display}) => ({
+            display: !display
+          }))}
+          className="p-3"
+        >
+          Mostrar atributos
+          <ArrowDown className={`${ display && 'rotate-180'}`} />
+        </button>
+        <div className={`overflow-hidden transition-all duration-300 ease-out
+        ${ display ? 'display' : 'hide'}`}>
+          <table>
+            <tbody>
+              { attributes.map(({ name, value_name, id }) => (
+                <tr key={id}>
+                  <td>{name}</td>
+                  <td>{value_name}</td>
+                </tr>
+              )) }
+            </tbody>
+          </table>
+        </div>
+      </div>
     );
   }
 }
