6799bf76ce8c0970f3ce0f6d01fcdd63bbbedd38
gralmeidan <gralmeidan@gmail.com>
2022-07-06 17:46:40 -0400

    Adapted the header for smaller screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,28 +15,32 @@ class Header extends React.Component {
     }));
   };
 
-  render = () => (
-    <header className='bg-rose-600 flex sticky top-0 z-30'>
-      <Link to='/'>
-        <h1 className='text-white text-2xl p-3 font-semibold'>TrybeStore</h1>
-      </Link>
-      <div className='flex grow justify-between'>
-        <div className='flex grow justify-center'>
-          {this.props.children}
+  render = () => {
+    return (
+      <header className='bg-rose-600 flex sticky top-0 z-30 overflow-hidden'>
+        <Link to='/'>
+          <h1 className='text-white text-2xl p-3 font-semibold'>
+            <span className='sm:hidden block'>T</span>
+            <span className='hidden sm:block'>TrybeStore</span>
+          </h1>
+        </Link>
+        <div className='flex grow justify-between'>
+          <div className='flex grow justify-center'>
+            {this.props.children}
+          </div>
+          <button onClick={this.modalToggle} className="mx-3">
+            <ShoppingCartIcon className='text-rose-50'/>
+          </button>
         </div>
-        <button onClick={this.modalToggle} className="mr-3">
-          <ShoppingCartIcon className='text-rose-50'/>
-        </button>
-      </div>
-      <CartModal 
-        display={this.state.display}
-        cartItems={this.props.cartItems}
-        modalToggle={this.modalToggle}
-        addToCart={this.props.addToCart}
-        removeFromCart={this.props.removeFromCart}
-      />
-    </header>
-  );
+        <CartModal 
+          display={this.state.display}
+          cartItems={this.props.cartItems}
+          modalToggle={this.modalToggle}
+          addToCart={this.props.addToCart}
+          removeFromCart={this.props.removeFromCart}
+        />
+      </header>
+    );};
 }
 
 Header.propTypes = {
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,11 +4,11 @@ import SearchIcon from '@mui/icons-material/Search';
 class SearchBar extends React.Component {
   render() {
     return(
-      <div className='self-center bg-rose-500 h-8 w-3/4 rounded-xl flex
+      <div className='self-center bg-rose-500 h-8 w-full sm:w-3/4 rounded-xl flex
       items-center px-2 text-white'>
         <input type='text' className='grow bg-transparent 
         focus-visible:outline-none mr-1 font-semibold
-        font-roboto'
+        font-roboto w-full'
         {...this.props}
         />
         <SearchIcon/>
